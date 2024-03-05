d511ff1f4554b68ac56ca29a24a40d3126b0b754
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-05 19:57:35 +0300

    feat: Helper functions

--- onchain/src/processor/helper.rs
+++ onchain/src/processor/helper.rs
@@ -3,6 +3,7 @@ use solana_program::{
     program_error::ProgramError
 };
 use borsh::BorshDeserialize;
+use crate::state::{Mantra, Account};
 
 pub fn get_pda_for_accounts(
     program_id: &Pubkey
@@ -17,4 +18,15 @@ pub fn my_try_from_slice_unchecked<T: BorshDeserialize>(data: &[u8]) -> Result<T
         Ok(result) => Ok(result),
         Err(_) => Err(ProgramError::InvalidInstructionData)
     }
+}
+
+pub fn get_mantra_size(mantra: &Mantra) -> usize{
+    let mut mantra_size = 4;
+
+    // Get size of all accounts
+    for x in mantra.accounts.iter() {
+        mantra_size += ((4 + x.manager_id.len()) + (4 + x.user_id.len()));
+    }
+
+    mantra_size
 }
