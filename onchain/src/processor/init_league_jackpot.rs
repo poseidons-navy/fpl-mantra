bdaaeace42a69dfdb5255bcc919047b9838515a1
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-07 16:03:00 +0300

    feat: v1 of function signature

---
+++ onchain/src/processor/init_league_jackpot.rs
@@ -0,0 +1,15 @@
+use solana_program::{
+    account_info::AccountInfo, 
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+pub fn init_league_jackpot(
+    league_name: String,
+    accounts: &[AccountInfo],
+    program_id: &Pubkey
+) -> ProgramResult {
+    
+    
+    Ok(())
+}
