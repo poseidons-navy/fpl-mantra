67f50aad1d1ab002e153427a958dc76894148159
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-07 16:07:04 +0300

    feat: function for getting address of jackpot wallet account of league

--- onchain/src/processor/helper.rs
+++ onchain/src/processor/helper.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 use solana_program::{
     pubkey::Pubkey,
     program_error::ProgramError
@@ -10,6 +12,17 @@ pub fn get_pda_for_accounts(
 ) -> (Pubkey, u8) {
     let (pda, bump_seed) = Pubkey::find_program_address(&["accounts".as_ref()], program_id);
     (pda, bump_seed)
+}
+
+pub fn get_league_jackpot_account(
+    league_name: String,
+    program_id: &Pubkey
+) -> (Pubkey, u8) {
+    let (jackpot, bump_seed) = Pubkey::find_program_address(
+        &[league_name.as_ref(), "jackpot_account".as_ref()], 
+        program_id
+    );
+    (jackpot, bump_seed)
 }
 
 pub fn my_try_from_slice_unchecked<T: BorshDeserialize>(data: &[u8]) -> Result<T, ProgramError> {
