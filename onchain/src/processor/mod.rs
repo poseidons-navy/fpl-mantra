13219bef91f28e5afe854826ede42f886bff2ec5
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-12 21:22:14 +0300

    feat: Create Competition

--- onchain/src/processor/mod.rs
+++ onchain/src/processor/mod.rs
@@ -2,3 +2,4 @@ pub mod create_league;
 pub mod create_account;
 pub mod helper;
 pub mod init_league_jackpot;
+pub mod create_competition;
--- onchain/src/state.rs
+++ onchain/src/state.rs
@@ -5,3 +5,11 @@ pub struct Account {
     pub user_id: String,
     pub manager_id: String
 }
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Competition {
+    pub members: Vec<String>,
+    pub name: String,
+    pub league_id: String,
+    pub entry_fee: f64
+}
