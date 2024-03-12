09365c0837c642b7d6b2f11debcc554da6c04447
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-12 22:44:31 +0300

    feat: Create league competition test

--- onchain/src/processor/create_competition.rs
+++ onchain/src/processor/create_competition.rs
@@ -9,8 +9,9 @@ use solana_program::{
     sysvar::{rent::Rent, Sysvar},
     msg
 };
+use solana_sdk::borsh1::try_from_slice_unchecked;
 
-use crate::processor::helper;
+use crate::{processor::helper, pstate::LeagueAccountState};
 use crate::state::Competition;
 
 pub fn create_competition(
@@ -25,17 +26,37 @@ pub fn create_competition(
     let accounts_iter = &mut accounts.iter();
     let creator_account = next_account_info(accounts_iter)?;
     let competition_account = next_account_info(accounts_iter)?;
+    let league_account = next_account_info(accounts_iter)?;
     let system_program = next_account_info(accounts_iter)?;
 
     if league_id.clone() != String::from("none") {
         msg!("Community belongs to league");
 
-        msg!("TODO: Check if league exists");
+        msg!("Check if league exists");
+        let (league, _) = Pubkey::find_program_address(
+            &["leagues".as_bytes().as_ref(), league_id.as_bytes().as_ref()], 
+            program_id
+        );
+
+        if league_account.key.clone() != league {
+            msg!("Wrong League Account Created");
+            return Err(ProgramError::InvalidArgument);
+        }
+
         // Check if league exists
+        if league_account.data_is_empty() {
+            msg!("League Does Not Exist");
+            return  Err(ProgramError::InvalidAccountData);
+        }
 
-        msg!("TODO: Check if creator belongs to league");
-        // Check if creator belongs to league
-
+        // Get league from league_account
+        let league = try_from_slice_unchecked::<LeagueAccountState>(&league_account.data.borrow()).unwrap();
+        
+        msg!("Check if creator belongs to league");
+        if !league.league_members.contains(&creator_id.clone()) {
+            msg!("Creator Not Member of League");
+            return  Err(ProgramError::InvalidArgument);
+        }
     }
 
     // Check if community exists
@@ -104,12 +125,12 @@ pub fn create_competition(
 #[cfg(test)]
 mod tests {
     use {
-        super::*, crate::pinstruction::LeagueInstructionStruct, solana_program::instruction::{AccountMeta, Instruction}, solana_program_test::{processor, ProgramTest}, solana_sdk::{signature::Keypair, signer::Signer, system_program, transaction::Transaction}, std::borrow::Borrow
+        super::*, crate::pinstruction::LeagueInstructionStruct, solana_program::instruction::{AccountMeta, Instruction}, solana_program_test::{processor, ProgramTest}, solana_sdk::{signer::Signer, system_program, transaction::Transaction},
     };
     use borsh::BorshDeserialize;
 
     #[tokio::test]
-    async fn create_competition_test() {
+    async fn create_global_competition_test() {
         let name = String::from("Test Competition");
         let league_id = String::from("none");
         let entry_fee = 0.0;
@@ -133,7 +154,7 @@ mod tests {
 
         let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
             "FPL Mantra",
-            program_id,
+            program_id.clone(),
             processor!(crate::entrypoint::process_instruction)
         ).start().await;
 
@@ -143,8 +164,11 @@ mod tests {
             league_id: league_id.clone(),
             entry_fee: entry_fee.clone(),
         };
-        let (competition, bump_seed) = helper::get_competition_account(name.clone(), league_id.clone(), &program_id);
-
+        let (competition, _) = helper::get_competition_account(name.clone(), league_id.clone(), &program_id);
+        let (league, _) = Pubkey::find_program_address(
+            &["leagues".as_bytes().as_ref(), league_id.as_bytes().as_ref()], 
+            &program_id
+        );
 
 
         let mut transaction = Transaction::new_with_payer(
@@ -153,6 +177,123 @@ mod tests {
                 accounts: vec![
                     AccountMeta::new(payer.pubkey(), true),
                     AccountMeta::new(competition, false),
+                    AccountMeta::new(league, false),
+                    AccountMeta::new(system_program::id(), false)
+                ],
+                data: sink
+            }],
+            Some(&payer.pubkey())
+        );
+
+        transaction.sign(&[&payer], recent_blockhash);
+
+        banks_client.process_transaction(transaction).await.unwrap();
+
+        let created_competition = banks_client.get_account(competition).await;
+
+        match created_competition {
+            Ok(None) => assert_eq!(false, true),
+            Ok(Some(account)) => {
+                let competition_account = Competition::deserialize(&mut account.data.to_vec().as_ref()).unwrap();
+
+                assert!(competition_account.members == competition_test.members.clone(), "Competition Created With Wrong Members");
+                assert!(competition_account.entry_fee == competition_test.entry_fee, "Competition Created With Wrong Entry Fee");
+                assert!(competition_account.league_id == competition_test.league_id.clone(), "Competition Created With Wrong League ID");
+                assert!(competition_account.name == competition_test.name, "Competition Created With Wrong Name");
+            },
+            Err(_) => assert_eq!(false, true)
+        }
+    }
+
+    #[tokio::test]
+    async fn create_league_competition_test() {
+        let league_name = String::from("TestLeague");
+        let league_id = String::from("1");
+        let entry_fee = 0.0;
+        let mut creator_id = String::from("1");
+        let events_included: u8 = 1;
+
+        // Create league
+        let mut instruction_data = LeagueInstructionStruct {
+            league_id: league_id.clone(),
+            creator_id: creator_id.clone(),
+            league_name: league_name.clone(),
+            events_included: events_included.clone(),
+            user_id: String::from(""),
+            manager_id: String::from(""),
+            entry_fee: entry_fee.clone(),
+            name: String::from("")
+        };
+        // Serialize instructions
+        let mut sink = vec![0];
+        instruction_data.serialize( &mut sink).unwrap();
+        let program_id = Pubkey::new_unique();
+
+        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
+            "FPL Mantra",
+            program_id.clone(),
+            processor!(crate::entrypoint::process_instruction)
+        ).start().await;
+
+        let (league, _) = Pubkey::find_program_address(
+            &["leagues".as_bytes().as_ref(), league_id.as_bytes().as_ref()], 
+            &program_id
+        );
+
+        let mut transaction = Transaction::new_with_payer(
+            &[Instruction {
+                program_id,
+                accounts: vec![
+                    AccountMeta::new(payer.pubkey(), true),
+                    AccountMeta::new(league, false),
+                    AccountMeta::new(system_program::id(), false)
+                ],
+                data: sink
+            }],
+            Some(&payer.pubkey())
+        );
+
+        transaction.sign(&[&payer], recent_blockhash);
+
+        banks_client.process_transaction(transaction).await.unwrap();
+
+        // Create competition
+        let name = String::from("Test Competition");
+        creator_id = String::from("1");
+        instruction_data = LeagueInstructionStruct {
+            league_id: league_id.clone(),
+            creator_id: creator_id.clone(),
+            league_name: String::from(""),
+            events_included: 0,
+            user_id: String::from(""),
+            manager_id: String::from(""),
+            entry_fee: entry_fee.clone(),
+            name: name.clone()
+        };
+
+        // Serialize instructions
+        sink = vec![4];
+        instruction_data.serialize( &mut sink).unwrap();
+
+        
+
+        let competition_test = Competition {
+            members: vec![creator_id.clone()],
+            name: name.clone(),
+            league_id: league_id.clone(),
+            entry_fee: entry_fee.clone(),
+        };
+        let (competition, _) = helper::get_competition_account(name.clone(), league_id.clone(), &program_id);
+        
+
+
+        transaction = Transaction::new_with_payer(
+            &[Instruction {
+                program_id,
+                accounts: vec![
+                    AccountMeta::new(payer.pubkey(), true),
+                    AccountMeta::new(competition, false),
+                    AccountMeta::new(league, false),
                     AccountMeta::new(system_program::id(), false)
                 ],
                 data: sink
