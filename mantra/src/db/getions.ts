2900ffce0a8ab9160570171732d38746b59c68d5
Roman Njoroge <roman.njoroge@njuguna.com>
2024-04-06 10:29:38 +0300

    feat: Updated to use my env variables

--- mantra/src/db/getions.ts
+++ mantra/src/db/getions.ts
@@ -1,5 +1,7 @@
 import { db } from "./creations";
-import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs, query, where, DocumentData } from "firebase/firestore";
+import "dotenv/config";
+import {FirebaseCompetition} from "@/utils/firebase-types";
 //HINT: If error occurs remember to check league_id that is changed to string from nuber
 /**
  * @param league_id
@@ -15,18 +17,21 @@ export async function getAllLeagueMembers(league_id: string) {
   }
 }
 
-export async function getLeaguesOfMember(member_id: string){
-  try {
-    const leagues = doc(db, "league_members", member_id);
-    const leagues_snapshot = await getDoc(leagues);
-    const leagues_data = leagues_snapshot.data();
-    console.log(leagues_data);
-    return [];
-  } catch(err) {
-    console.log(err);
-    throw "Could Not Get Leagues Of User"
-  }
-}
+// export async function getLeaguesOfMember(member_id: string): Promise<FirebaseCompetition[]>{
+//   try {
+//     const q = query(collection(db, "league_members"), where("member_id", "==", member_id));
+//     const querySnapshot = await getDocs(q);
+//     const member_leagues: FirebaseCompetition[] = [];
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, "=>", doc.data());
+//    });
+//    return member_leagues;
+//   } catch(err) {
+//     console.log(err);
+//     throw "Could Not Get Leagues Of User"
+//   }
+// }
 
 export async function getLeagues() {
   try {
@@ -40,12 +45,14 @@ export async function getLeagues() {
   }
 }
 
-export async function getCompetitionsFromDB() {
+export async function getCompetitionsFromDB(): Promise<FirebaseCompetition[]> {
   try {
+    console.log("Getting Competitions FROM DB");
     const competitionCollection = collection(db, "competition");
     const competitionsSnapshot = await getDocs(competitionCollection);
-    const competitions = competitionsSnapshot.docs.map((doc) => doc.data());
-    return competitions;
+    const competitions = competitionsSnapshot.docs.map((doc) =>  FirebaseCompetition.fromFirebase(doc.data()));
+    console.log(competitions);
+    return [];
   } catch (e: any) {
     console.log("Could Not Get Competitions", e);
     throw new Error("Could Not Get Competitions From DB");
